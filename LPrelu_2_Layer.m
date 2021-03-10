b3d3e860b94ab57e22ef6c48045669028a982361
Md Tahmid Hossain <tahmid.hossain007@gmail.com>
2021-03-11 10:10:07 +1100

    Update LPrelu_2_Layer.m

--- LPrelu_2_Layer.m
+++ LPrelu_2_Layer.m
@@ -1,7 +1,10 @@
 classdef LPrelu_2_Layer < nnet.layer.Layer
-    %     properties (Learnable)
-    %         Cut
-    %     end
+         properties (Learnable)
+             A
+             B
+             Alpha
+             Beta
+         end
     methods
         function layer = LPrelu_2_Layer(numChannels, name)
             
@@ -11,14 +14,16 @@ classdef LPrelu_2_Layer < nnet.layer.Layer
             % Set layer description.
             layer.Description = "LPrelu_2_Layer with " + numChannels + " channels";
             
-            % layer.Cut = 10;
+            layer.A = 5;
+            layer.B = 8;
+            layer.Alpha = .05;
+            layer.Beta = .05/3;
         end
         
         function Z = predict(layer, X)
             
             Z =   (0 < X & X <= 5).*X + (5 < X & X <= 8).*(5 + 0.05.*X) + (8 < X).*(5.4 + 0.02.*X);
-            % Z =   (0 < X & X < 4).*X + (4 < X).*4; %clipped_ReLU
-            %Z =   (0 < X & X <= layer.Cut).*X + (layer.Cut < X).*(layer.Cut + 0.05.*X);
+            %Z =   (0 < X & X <= layer.A).*X + (layer.A < X & X <= layer.B).*(layer.A + layer.Alpha.*X) + (layer.B < X).*(5.4 + layer.Beta.*X);
         end
         
     end
